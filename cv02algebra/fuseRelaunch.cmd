42584255db23f2725e38715f148b9e482fd2e42d
Tomas Sykora, jr. <jump@syky.cz>
2015-02-24 17:08:49 +0100

    clean

--- cv02algebra/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "H:/BI-SAP/BI-SAP15/cv02algebra/l2_agebra_4_test_isim_beh.exe" -prj "H:/BI-SAP/BI-SAP15/cv02algebra/l2_agebra_4_test_beh.prj" "work.l2_agebra_4_test" 
