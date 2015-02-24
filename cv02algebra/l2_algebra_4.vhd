c5e380fb456ac3d909de1cef667ad05fae49c2de
Tomas Sykora, jr. <jump@syky.cz>
2015-02-24 17:07:01 +0100

    algebra

---
+++ cv02algebra/l2_algebra_4.vhd
@@ -0,0 +1,34 @@
+-- knihovna typù pro práci
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- definice vstupù a výstupù - vstupy a, b, c, výstupy f
+entity algebra is
+    Port ( d : in std_logic;
+			c : in std_logic;
+           b : in std_logic;
+           a : in std_logic;
+           f : out std_logic);
+end algebra;
+
+-- vlastní popis chování obvodu -> výpoèet funkce f
+architecture Behavioral of algebra is
+begin
+  -- ve výrazech použivejte následující operátory:
+  -- ----------------------------------
+  -- | operátor | pøíklad použití     |
+  -- ----------------------------------
+  -- |    and   |	a and b and c       |
+  -- |    or    |	a or b or (c and d) |
+  -- |    not   | not a               |
+  -- ----------------------------------
+  -- používejte závorky!
+  -- pøíklad:
+  --   zadání:                            f(a, b, c) = a * #b * #c + a * b
+  --   reprezentace na následující øádce: f <= (a and (not b) and (not c)) or (a and b)
+  -- funkce z návodu má tvar:
+  --f <= ((not a) and (not b) and a) or ((not c) and b and (not a)) or (c and (not b) and (not a)) or (c and b and (not a));
+  f <= (((not a) and c )or ((not a) and b) or (a and (not b)and(not c)));
+end Behavioral;
